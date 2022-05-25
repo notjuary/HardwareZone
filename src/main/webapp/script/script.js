dd04b8b0bd1a87cfaa8648839f0d15c022bf91e7
grauso-t <grauso.teo@gmail.com>
2022-05-26 00:39:53 +0200

    Miglioramenti

--- src/main/webapp/script/script.js
+++ src/main/webapp/script/script.js
@@ -1,7 +1,12 @@
 const info_string = /^([a-zA-Z\xE0\xE8\xE9\xF9\xF2\xEC\x27]\s?)+$/; /*caratteri, lettere accentate apostrofo e un solo spazio fra le parole*/
 const email_string = /^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; /*	caratteri e . _ % – + @ + caratteri compreso . + . + min 2, max 4 caratteri*/
-const password_String = /^[a-zA-Z0-9\_\*\-\+\!\?\,\:\;\.\xE0\xE8\xE9\xF9\xF2\xEC\x27]{6,12}/; /*min 6, max 12 di caratteri, numeri, _ * – + ! ? , : ; . e lettere accentate*/
+const password_string = /^[a-zA-Z0-9\_\*\-\+\!\?\,\:\;\.\xE0\xE8\xE9\xF9\xF2\xEC\x27]{6,12}/; /*min 6, max 12 di caratteri, numeri, _ * – + ! ? , : ; . e lettere accentate*/
+const phone_string = /^\d{10}$/ /*10 numeri*/
 const cap_string = /^\d{5}$/; /*5 numeri*/
+
+function showMenu() {
+
+}
 
 function showTabPersonalInformation() {
     $(".tab-personal-information").show();
@@ -31,18 +36,29 @@ function validatePersonalInformation() {
         showTabLoginInformation();
     }
     else {
-        alert("Non corretto");
+        if (!(info_string.test(nome)))
+            alert("Nome non valido");
+        if (!(info_string.test(cognome)))
+            alert("Cognome non valido");
+        if (data_nascita === "")
+            alert("Data non valida");
     }
 }
 function validateLoginInformation() {
 
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
+    let telefono = document.getElementById("telefono").value;
 
-    if (email_string.test(email) && password_String.test(password)) {
+    if (email_string.test(email) && password_string.test(password) && phone_string.test(telefono)) {
         showTabAddressInformation();
     }
     else {
-        alert("Non corretto");
+        if (!(info_string.test(email)))
+            alert("Email non valida");
+        if (!(info_string.test(password)))
+            alert("Password non valida<");
+        if (!(phone_string.test(telefono)))
+            alert("Telefono non valido");
     }
 }
