4ef6848babfa58ee03151f42c97ade6ec05a4311
grauso-t <grauso.teo@gmail.com>
2022-05-26 17:19:29 +0200

    Creazione DB

--- database/createDB.sql
+++ database/createDB.sql
@@ -4,59 +4,86 @@ CREATE DATABASE ecommerce;
 USE ecommerce;
 
 CREATE TABLE Utente (
-	ID INT PRIMARY KEY AUTO_INCREMENT,
-	Nome VARCHAR(30) NOT NULL,
-	Cognome VARCHAR(30) NOT NULL,
-	Email VARCHAR(30) NOT NULL,
-	Pass VARCHAR(30) NOT NULL,
-	Telefono VARCHAR(10) NOT NULL,
-	Via VARCHAR(30) NOT NULL,
-	Numero_Civico VARCHAR(3) NOT NULL,
-	CAP VARCHAR(3) NOT NULL,
-	Città VARCHAR(30) NOT NULL,
-    Data_Registrazione DATE NOT NULL,
-	Stato_Account INT DEFAULT 1 NOT NULL
+	ID_Utente INT AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(30) NOT NULL,
+    Cognome VARCHAR(30) NOT NULL,
+    Data_Nascita DATE NOT NULL,
+    Email VARCHAR(30) NOT NULL,
+    Accesso VARCHAR(12) NOT NULL,
+    Telefono CHAR(10) NOT NULL,
+    Città VARCHAR(30) NOT NULL,
+    Provincia CHAR(2) NOT NULL,
+    Codice_Postale CHAR(5) NOT NULL,
+    Indirizzo VARCHAR(10) NOT NULL,
+    Data_Registazione DATE NOT NULL,
+    Stato BOOLEAN NOT NULL DEFAULT TRUE
 );
 
-CREATE TABLE Ordine (
-	ID INT PRIMARY KEY AUTO_INCREMENT,
-	Totale DOUBLE,
-	Stato VARCHAR(10),
-	Data_Ordine DATE,
-	ID_Cliente INT,
-	FOREIGN KEY (ID_Cliente) REFERENCES Utente(ID)
+CREATE TABLE Amministratore (
+	ID_Admin INT AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(30) NOT NULL,
+    Cognome VARCHAR(30) NOT NULL,
+    Email VARCHAR(30) NOT NULL,
+    Accesso VARCHAR(12) NOT NULL
 );
 
-CREATE TABLE Spedizione (
-	ID_Spedizione INT PRIMARY KEY AUTO_INCREMENT,
-	Nome_Corriere VARCHAR(30),
-	URL_Monitoraggio VARCHAR(30)
+CREATE TABLE Carrello (
+	ID_Carrello INT AUTO_INCREMENT PRIMARY KEY,
+    Utente INT REFERENCES Utente(ID_Utente),
+    Totale DOUBLE NOT NULL,
+    Numero_Prodotti INT NOT NULL DEFAULT 0
 );
 
 CREATE TABLE Prodotto (
-	ID INT PRIMARY KEY AUTO_INCREMENT,
-	Nome VARCHAR(30),
-	Descrizione VARCHAR(256),
-	Prezzo DOUBLE,
-	Sconto DOUBLE,
-	Categoria VARCHAR(30),
-	Data_Aggiunto DATE,
-	Data_Aggiorna DATE,
-	Quantità INT
+	ID_Prodotto INT AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(30) NOT NULL,
+    Descrizione VARCHAR(255) NOT NULL,
+    Prezzo DOUBLE NOT NULL,
+    Quantità_Disponibile INT NOT NULL DEFAULT 0,
+    Sconto INT NOT NULL DEFAULT 0,
+    Immagine VARCHAR(255) NOT NULL,
+    Categoria VARCHAR(10) NOT NULL
 );
 
-CREATE TABLE C_P(
-	ID INT PRIMARY KEY AUTO_INCREMENT,
-	ID_Carrello INT,
-	ID_Prodotto INT,
-	FOREIGN KEY (ID_Carrello) REFERENCES Carrello(ID),
-	FOREIGN KEY (ID_Prodotto) REFERENCES Prodotto(ID)
+CREATE TABLE Carrello_Prodotto (
+	ID_Carrello_Prodotto INT AUTO_INCREMENT PRIMARY KEY,
+    Prodotto INT REFERENCES Prodotto(ID_Prodotto),
+    Carrello INT REFERENCES Carrello(ID_Carrello),
+    Quantità INT NOT NULL DEFAULT 1
 );
 
-CREATE TABLE O_P(
-	ID INT PRIMARY KEY AUTO_INCREMENT,
-	ID_Ordine INT,
-	ID_prodotto INT,
-	FOREIGN KEY (ID_Ordine) REFERENCES Ordine(ID),
-	FOREIGN KEY (ID_Prodotto) REFERENCES Prodotto(ID)
+CREATE TABLE Ordine (
+	ID_Ordine INT AUTO_INCREMENT PRIMARY KEY,
+    Utente INT REFERENCES Utente(ID_Utente),
+    Costi_Spedizione DOUBLE,
+    Totale DOUBLE NOT NULL
+);
+
+CREATE TABLE Ordine_Prodotto (
+	ID_Ordine_Prodotto INT AUTO_INCREMENT PRIMARY KEY,
+	Ordine INT REFERENCES Ordine(ID_Ordine),
+    Prodotto INT REFERENCES Prodotto(ID_Prodotto)
+);
+
+CREATE TABLE Spedizione (
+	ID_Spedizione INT AUTO_INCREMENT PRIMARY KEY,
+    Data_Partenza DATE NOT NULL,
+    Data_Arrivo DATE NOT NULL,
+    Ordine INT REFERENCES Ordine(ID_Ordine)
+);
+
+CREATE TABLE Pagamento (
+	ID_Pagamento INT AUTO_INCREMENT PRIMARY KEY,
+    Ordine INT REFERENCES Ordine(ID_Ordine),
+    Data_Pagamento DATE NOT NULL,
+    Stato BOOLEAN DEFAULT 0 NOT NULL
+);
+
+CREATE TABLE Fattura (
+	ID_Fattura INT AUTO_INCREMENT PRIMARY KEY,
+	ID_Pagamento INT REFERENCES Pagamento(ID_Pagamento),
+	Città VARCHAR(30) NOT NULL,
+    Provincia CHAR(2) NOT NULL,
+    Codice_Postale CHAR(5) NOT NULL,
+    Indirizzo VARCHAR(10) NOT NULL
 );
